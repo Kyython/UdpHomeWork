2ae92c2141d910209ca665c93a521ee5ed55f83b
Асанали Ерик <asanali2001@mail.ru>
2019-06-26 14:58:16 +0600

    Home Work

---
+++ Client-Udp/Client-Udp/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Client_Udp
+{
+    public partial class MainWindow : Window
+    {
+        private UdpClient _client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            int localPort = 12345;
+            string ipAddress = "192.168.56.1";
+
+        //    try
+        //    {
+                _client = new UdpClient(new IPEndPoint(IPAddress.Parse(ipAddress), localPort));
+         //   }
+         //   catch (Exception exception)
+         //   {
+          //      MessageBox.Show($"Error: {exception.Message}");
+          //  }
+        }
+
+        private void EnterButtonClick(object sender, RoutedEventArgs e)
+        {
+            const int OFFSET = 0;
+            int remotePort = 54321;
+
+            ThreadPool.QueueUserWorkItem((state) =>
+            {
+                var ipEndPoint = new IPEndPoint(IPAddress.Any, remotePort);
+
+            //    try
+            //    {
+                    byte[] buffer = _client.Receive(ref ipEndPoint);
+                    using (var memoryStream = new MemoryStream(buffer, OFFSET, buffer.Length))
+                    {
+                        memoryStream.Write(buffer, OFFSET, buffer.Length);
+                        Dispatcher.Invoke(new Action(() =>
+                        {
+                            screenshotImage.Source = BitmapFrame.Create(memoryStream, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+                        }), System.Windows.Threading.DispatcherPriority.Background);
+                    }
+                //}
+              //  catch (Exception exception)
+              //  {
+               //     MessageBox.Show($"Error: {exception.Message}");
+               // }
+            });
+        }
+    }
+}
---
+++ Server-Udp/Server-Udp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Server_Udp
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem((state) =>
+            {
+                CreateScreenshot();
+                SendScreenshot();
+            });
+
+            Console.ReadLine();
+        }
+
+        private static void CreateScreenshot()
+        {
+            const int COORDINATE = 0;
+
+            var screenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            using (var graphics = Graphics.FromImage(screenshot))
+            {
+                graphics.CopyFromScreen(COORDINATE, COORDINATE, COORDINATE, COORDINATE, Screen.PrimaryScreen.Bounds.Size);
+                screenshot.Save("UserScreenshot.png");
+            }
+        }
+
+        private static void SendScreenshot()
+        {
+            const int BUFFER_SIZE = 64512;
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int localPort = 54321;
+            int remotePort = 12345;
+            string ipAddress = "192.168.56.1";
+
+            try
+            {
+                using (var fileStream = new FileStream("UserScreenshot.png", FileMode.Open, FileAccess.Read))
+                using (var binaryReader = new BinaryReader(fileStream))
+                {
+                    buffer = binaryReader.ReadBytes((int)fileStream.Length);
+                }
+
+                using (var server = new UdpClient(new IPEndPoint(IPAddress.Any, localPort)))
+                {
+                    server.Connect(new IPEndPoint(IPAddress.Parse(ipAddress), remotePort));
+                    server.Send(buffer, BUFFER_SIZE);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Error: {exception.Message}");
+            }
+        }      
+    }
+}
